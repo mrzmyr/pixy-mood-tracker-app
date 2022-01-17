4aea05095215c189bbefa22a901659cde198f97e
mrzmyr <moritz@devmood.com>
2022-01-17 11:06:07 +0100

    fix(calendar): start week on mondays

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -12,6 +12,7 @@ export default function Calendar() {
     <>
       <CalendarHeader />
       <CalendarList
+        firstDay={1}
         theme={{
           calendarBackground: colors.background,
           monthTextColor: colors.calendarMonthNameColor,
