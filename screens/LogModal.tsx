8680a968f64778e1fa2b8f27167586fe121a17b8
mrzmyr <moritz@devmood.com>
2022-01-23 13:38:55 +0100

    chore: line indentations

--- screens/LogModal.tsx
+++ screens/LogModal.tsx
@@ -20,13 +20,13 @@ function ModalHeader({
   
   return (
     <View
-    style={{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginBottom: 30,
-    }}
-  >
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 30,
+      }}
+    >
     <View style={{
       justifyContent: 'flex-start',
       flexDirection: 'row',
@@ -104,7 +104,10 @@ export default function LogModal({ navigation, route }) {
       flex: 1,
       justifyContent: 'flex-start',
       backgroundColor: colors.background,
-      padding: 20,
+      paddingTop: 15,
+      paddingBottom: 15,
+      paddingLeft: 20,
+      paddingRight: 20,
       marginTop: Platform.OS === 'ios' ? 0 : 40,
     }}>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
@@ -139,15 +142,19 @@ export default function LogModal({ navigation, route }) {
         autoFocus
       />
       {existingLogItem && (
-      <View
-        style={{
-          marginTop: 20,
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}
-      >
-        <LinkButton onPress={remove} type='secondary' icon={<Trash2 width={16} color={colors.secondaryLinkButtonText} />}>{i18n.t('delete')}</LinkButton>
-      </View>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <LinkButton 
+            onPress={remove} 
+            type='secondary' 
+            icon={<Trash2 width={16} color={colors.secondaryLinkButtonText} />}
+          >{i18n.t('delete')}</LinkButton>
+        </View>
       )}
     </View>
     </DismissKeyboard>
