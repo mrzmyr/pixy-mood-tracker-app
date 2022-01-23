d5da6baf1fbb4ccdcb686c23a5f0f59db61f2aac
mrzmyr <moritz@devmood.com>
2022-01-23 13:37:37 +0100

    style(Button): remove margin and flex, add icon

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,19 @@
-import { Pressable, Text } from "react-native";
-import Colors from "../constants/Colors";
+import { Pressable, Text, View } from "react-native";
 import useColors from "../hooks/useColors";
 
-export default function Button({ type = 'primary', onPress, children, style = {} }) {
+export default function Button({ 
+  type = 'primary', 
+  icon,
+  onPress, 
+  children, 
+  style = {} 
+}: {
+  type?: 'primary' | 'secondary',
+  icon?: React.ReactNode,
+  onPress?: () => void,
+  children: React.ReactNode,
+  style?: any
+}) {
   const colors = useColors()
   
   const buttonColors = {
@@ -19,17 +30,19 @@ export default function Button({ type = 'primary', onPress, children, style = {}
   return (
     <Pressable
       style={({ pressed }) => [{
-        flex: 1,
-        marginTop: 20,
-        padding: 15,
+        padding: 10,
+        paddingRight: 15,
+        paddingLeft: 15,
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
         borderRadius: 5,
         opacity: pressed ? 0.8 : 1,
         backgroundColor: buttonColors?.background,
       }, style]}
       onPress={onPress}
     >
+      {icon && <View style={{ marginRight: children ? 10 : 0 }}>{icon}</View>}
       <Text style={{ fontSize: 16, color: buttonColors?.text }}>{children}</Text>
     </Pressable>
   )
