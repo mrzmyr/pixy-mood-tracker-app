192e04a6bfbda54821ea336d6d63358fc3cd1348
mrzmyr <moritz@devmood.com>
2022-01-25 13:50:49 +0100

    chore: add all types to changelog

--- .versionrc.js
+++ .versionrc.js
@@ -16,4 +16,17 @@ module.exports = {
       updater: require.resolve('standard-version-expo/ios'),
     },
   ],
+  types: [
+    {type: "feat", section: "Features"},
+    {type: "fix", section: "Bug Fixes"},
+    {type: "docs", section: "Documentation"},
+    {type: "style", section: "Styling"},
+    {type: "refactor", section: "Refactors"},
+    {type: "perf", section: "Performance"},
+    {type: "test", section: "Tests"},
+    {type: "build", section: "Build System"},
+    {type: "ci", section: "CI"},
+    {type: "chore", section: "Chore"},
+    {type: "revert", section: "Reverts"}
+  ]
 };
