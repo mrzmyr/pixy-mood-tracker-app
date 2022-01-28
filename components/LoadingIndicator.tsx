45dacce1fc82faf9af564c60864a2fe6f06e70d3
mrzmyr <moritz@devmood.com>
2022-01-28 21:45:22 +0100

    feat(LoadingIndicator): add new component

---
+++ components/LoadingIndicator.tsx
@@ -0,0 +1,43 @@
+import { Animated, Easing } from "react-native";
+import { Loader } from "react-native-feather";
+
+export default function LoadingIndicator({
+  size = 20,
+  color,
+}: {
+  size: number,
+  color: string,
+}) {
+  let spinValue = new Animated.Value(0);
+
+  Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }),
+    {iterations: -1},
+  ).start();
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  })
+
+  return (
+    <Animated.View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: size,
+        height: size,
+        transform: [{
+          rotate: spin
+        }]
+      }}
+    >
+      <Loader color={color} width={size} />
+    </Animated.View>
+  )
+}
