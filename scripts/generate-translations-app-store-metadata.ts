7853ea5db49cb0d602a5f973ca40d3daaf0af835
mrzmyr <moritz@devmood.com>
2022-01-29 23:23:08 +0100

    feat: add new translations script for app store metadata

---
+++ scripts/generate-translations-app-store-metadata.ts
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import { translate } from './utils';
+const { readDirYAML, JSON2YML } = require('./utils');
+
+const dir = __dirname + '/app-store-metadata/';
+const locales = readDirYAML(dir);
+
+const KEY_BLACK_LIST = [
+  'support_url',
+]
+
+type MissingKeyMap = {
+  [key: string]: string[]
+}
+
+const missingKeyMap: MissingKeyMap = {};
+
+for(const localeKey of Object.keys(locales).filter(key => key !== 'en')) {
+  for(const templateKey of Object.keys(locales.en)) {
+    if(!missingKeyMap[localeKey]) missingKeyMap[localeKey] = [];
+    if(!Object.keys(locales[localeKey]).includes(templateKey)) {
+      missingKeyMap[localeKey].push(templateKey);
+    }
+  }
+}
+
+(async () => {
+  const result: {
+    [locale: string]: {
+      [key: string]: string
+    }
+  } = {};
+
+  for(const localeKey in missingKeyMap) {
+    for(const key of missingKeyMap[localeKey]) {
+      if(!result[localeKey]) result[localeKey] = {}
+      if(KEY_BLACK_LIST.includes(key)) {
+        result[localeKey][key] = locales.en[key];
+      } else {
+        console.log('translating: ', localeKey, key, '…')
+        result[localeKey][key] = await translate({
+          text: locales.en[key], 
+          target: localeKey 
+        })
+      }
+    }
+
+    const locale = {
+      ...locales[localeKey],
+      ...result[localeKey],
+    };
+    fs.writeFileSync(`${dir}${localeKey}.yml`, JSON2YML(locale));
+    console.log('Saved', localeKey, '!')
+  }
+})()
