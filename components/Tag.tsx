005dc86143aae0d1e4474ae7345abf6b8e887ca7
mrzmyr <moritz@devmood.com>
2022-09-19 21:29:44 +0200

    chore: remove unused component

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,34 +1,5 @@
-import { Pressable, Text, TouchableOpacity, View } from "react-native";
-import { Check } from "react-native-feather";
+import { Text, TouchableOpacity } from "react-native";
 import useColors from "../hooks/useColors";
-
-const Checkbox = ({
-  colorName,
-  checked,
-}: {
-  colorName: string;
-  checked: boolean;
-}) => {
-  const colors = useColors();
-
-  return (
-    <View
-      style={{
-        padding: 4,
-        borderWidth: 2,
-        borderColor: checked ? colors.tags[colorName]?.background : colors.tags[colorName]?.border,
-        width: 22,
-        height: 22,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-        backgroundColor: checked ? colors.tags[colorName]?.background : colors.tags[colorName]?.text,
-      }}
-    >
-      {checked && <Check strokeWidth={3} width={12} color={colors.tags[colorName]?.text} />}
-    </View>
-  );
-};
 
 export default function Tag({ 
   colorName,
