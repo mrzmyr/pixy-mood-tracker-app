84ba01e1229af9a5ee3d2286e27326c5dd321631
mrzmyr <moritz@devmood.com>
2022-09-26 12:28:11 +0200

    fix: remove tags `title` from tags_distribution feedback report

--- screens/Statistics/TagsDistributionCard.tsx
+++ screens/Statistics/TagsDistributionCard.tsx
@@ -92,7 +92,7 @@ export const TagsDistributionCard = ({
           </View>
         )}
       </View>
-      <CardFeedback type='tags_distribution' details={{ count: itemsWithTags.length, tags: tags.map(d => _.omit(d, 'title')) }} />
+      <CardFeedback type='tags_distribution' details={{ count: itemsWithTags.length, tags: tags.map(d => _.omit(d.details, 'title')) }} />
     </Card>
   );
 };
