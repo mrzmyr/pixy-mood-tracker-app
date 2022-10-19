74dcdd91e21b6229c316f4770d414b793c55058e
mrzmyr <moritz@devmood.com>
2022-10-19 14:25:35 +0200

    feat(TagsDistribution): click to open filter view

--- screens/Statistics/TagsDistributionCard.tsx
+++ screens/Statistics/TagsDistributionCard.tsx
@@ -1,10 +1,14 @@
+import { useNavigation } from '@react-navigation/native';
 import _ from 'lodash';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { Card } from '../../components/Statistics/Card';
 import { t } from '../../helpers/translation';
 import { useAnonymizer } from '../../hooks/useAnonymizer';
+import { useCalendarFilters } from '../../hooks/useCalendarFilters';
 import useColors from '../../hooks/useColors';
+import useHaptics from '../../hooks/useHaptics';
 import { TagsDistributionData } from '../../hooks/useStatistics/TagsDistribution';
+import { Tag } from '../../hooks/useTags';
 import { CardFeedback } from './CardFeedback';
 
 export const TagsDistributionCard = ({
@@ -13,7 +17,19 @@ export const TagsDistributionCard = ({
   data: TagsDistributionData
 }) => {
   const colors = useColors()
+  const haptic = useHaptics()
   const { anonymizeTag } = useAnonymizer()
+  const calendarFilters = useCalendarFilters()
+  const navigation = useNavigation()
+  
+  const onPress = (tagId: Tag['id']) => {
+    haptic.selection()
+    calendarFilters.set({
+      ...calendarFilters.data,
+      tagIds: [tagId],
+    })
+    navigation.navigate('Calendar')
+  }
   
   return (
     <Card
@@ -30,11 +46,12 @@ export const TagsDistributionCard = ({
       >
         {data.tags.slice(0, 5).map(tag => {
           return (
-            <View
+            <Pressable
               key={tag?.details?.id}
+              onPress={() => onPress(tag.id)}
               style={{
                 position: 'relative',
-                height: 24,
+                height: 32,
                 marginBottom: 8,
                 justifyContent: 'center',
                 paddingLeft: 8,
@@ -43,7 +60,7 @@ export const TagsDistributionCard = ({
               <View
                 style={{
                   backgroundColor: colors.tags[tag?.details?.color]?.background,
-                  height: 24,
+                  height: 32,
                   width: tag.count / data.tags[0].count * 100 + '%',
                   borderRadius: 4,
                   position: 'absolute',
@@ -57,7 +74,7 @@ export const TagsDistributionCard = ({
                   fontWeight: '600',
                 }}
               >{tag.count}x {tag?.details?.title}</Text>
-            </View>
+            </Pressable>
           );
         })}
         {data.tags.length > 5 && (
