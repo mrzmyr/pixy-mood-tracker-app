edbb722ceb3e3b788d633d2c79ad0f5534bf242a
mrzmyr <moritz@devmood.com>
2022-10-24 11:12:29 +0200

    feat: translate calendar hint

--- screens/Calendar/Calendar.tsx
+++ screens/Calendar/Calendar.tsx
@@ -11,6 +11,7 @@ import CalendarMonth from "./CalendarMonth";
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import { useStyle } from "react-native-style-utilities";
+import { t } from "../../helpers/translation";
 import { DATE_FORMAT } from "../../constants/Config";
 
 dayjs.extend(isSameOrBefore)
@@ -20,12 +21,12 @@ const MONTH_COUNT = 12;
 const MONTH_ITEMS: (Omit<LogItem, 'message'> & { messageLength: number })[][] = []
 const MONTH_ITEMS_FILTERED: LogItem[][] = []
 
-for(let i = 0; i < MONTH_COUNT; i++) {
+for (let i = 0; i < MONTH_COUNT; i++) {
   MONTH_ITEMS.push([])
   MONTH_ITEMS_FILTERED.push([])
 }
 
-const Calendar = memo(forwardRef(function Calendar({}, ref: React.RefObject<View>) {
+const Calendar = memo(forwardRef(function Calendar({ }, ref: React.RefObject<View>) {
   const colors = useColors()
   const logState = useLogState()
   const today = dayjs();
@@ -33,20 +34,20 @@ const Calendar = memo(forwardRef(function Calendar({}, ref: React.RefObject<View
   const calendarFilters = useCalendarFilters()
 
   const monthItems = useMemo(() => {
-    
-    for(let i = 0; i < MONTH_ITEMS.length; i++) {
+
+    for (let i = 0; i < MONTH_ITEMS.length; i++) {
       const start = today.clone().subtract(i, 'month').startOf('month');
       const end = today.clone().subtract(i, 'month').endOf('month');
       const _items = Object.values(logState.items)
         .filter(item => (
-          dayjs(item.date).isSameOrAfter(start, 'day') && 
+          dayjs(item.date).isSameOrAfter(start, 'day') &&
           dayjs(item.date).isSameOrBefore(end, 'day')
         ))
         .map(item => ({
           ..._.omit(item, ['tags', 'message']),
           messageLength: item.message.length,
         }))
-      if(!_.isEqual(MONTH_ITEMS[i], _items)) {
+      if (!_.isEqual(MONTH_ITEMS[i], _items)) {
         MONTH_ITEMS[i] = _items;
       }
     }
@@ -55,23 +56,23 @@ const Calendar = memo(forwardRef(function Calendar({}, ref: React.RefObject<View
   }, [JSON.stringify(logState.items), today.format(DATE_FORMAT)]);
 
   const monthItemsFiltered = useMemo(() => {
-    
-    for(let i = 0; i < MONTH_ITEMS.length; i++) {
+
+    for (let i = 0; i < MONTH_ITEMS.length; i++) {
       const start = today.clone().subtract(i, 'month').startOf('month');
       const end = today.clone().subtract(i, 'month').endOf('month');
       const _items = calendarFilters.data.filteredItems
         .filter(item => (
-          dayjs(item.date).isSameOrAfter(start, 'day') && 
+          dayjs(item.date).isSameOrAfter(start, 'day') &&
           dayjs(item.date).isSameOrBefore(end, 'day')
         ))
-      if(!_.isEqual(MONTH_ITEMS_FILTERED[i], _items)) {
+      if (!_.isEqual(MONTH_ITEMS_FILTERED[i], _items)) {
         MONTH_ITEMS_FILTERED[i] = _items;
       }
     }
 
     return MONTH_ITEMS_FILTERED;
   }, [JSON.stringify(calendarFilters.data.filteredItems)]);
-  
+
   const bottomTextStyles = useStyle(() => [
     styles.bottomText,
     {
@@ -85,8 +86,8 @@ const Calendar = memo(forwardRef(function Calendar({}, ref: React.RefObject<View
       style={styles.container}
     >
       {monthItems.map((items, index) => (
-        <CalendarMonth 
-          key={index} 
+        <CalendarMonth
+          key={index}
           dateString={today.clone().startOf('month').subtract(monthItems.length - (index + 1), 'month').format(DATE_FORMAT)}
           items={monthItems[monthItems.length - (index + 1)]}
           filteredItems={monthItemsFiltered[monthItems.length - (index + 1)]}
@@ -94,7 +95,7 @@ const Calendar = memo(forwardRef(function Calendar({}, ref: React.RefObject<View
           scaleType={settings.scaleType}
         />
       ))}
-      <Text style={bottomTextStyles}>🙏 The future will be great.</Text>
+      <Text style={bottomTextStyles}>🙏 {t('calendar_bottom_hint')}</Text>
     </View>
   )
 }))
--- scripts/generate-translations-app.js
+++ scripts/generate-translations-app.js
@@ -1,74 +1,86 @@
-const {Translate} = require('@google-cloud/translate').v2;
-const fs = require('fs');
-const credentials = require('../credentials/google-cloud-service-account.json');
+const { Translate } = require("@google-cloud/translate").v2;
+const fs = require("fs");
+const credentials = require("../credentials/google-cloud-service-account.json");
 
-const path = __dirname + '/../assets/locales/';
-const filesArray = fs.readdirSync(path).filter(file => fs.lstatSync(path+file).isFile())
+const path = __dirname + "/../assets/locales/";
+const filesArray = fs
+  .readdirSync(path)
+  .filter((file) => fs.lstatSync(path + file).isFile());
 const locales = {};
-filesArray.forEach(file => {
-  console.log('reading', file)
+filesArray.forEach((file) => {
+  console.log("reading", file);
   try {
-    locales[file.replace('.json', '')] = JSON.parse(fs.readFileSync(path+file, 'utf8'))
+    locales[file.replace(".json", "")] = JSON.parse(
+      fs.readFileSync(path + file, "utf8")
+    );
   } catch (e) {
-    console.log('error reading', file)
+    console.log("error reading", file);
   }
 });
 
-const FORCE_KEYS = ['reset_data_confirm_title', 'reset_data_confirm_message', 'reset_data_success_title', 'reset_data_success_message']
+const FORCE_KEYS = [];
 
 const missingKeys = {};
 
 const enKeys = Object.keys(locales.en);
 
-enKeys.forEach(key => {
+enKeys.forEach((key) => {
   // ['zh'].forEach(localeKey => {
-  Object.keys(locales).forEach(localeKey => {
-    if(!missingKeys[localeKey]) missingKeys[localeKey] = [];
-    if(!Object.keys(locales[localeKey]).includes(key) || FORCE_KEYS.includes(key)) {
+  Object.keys(locales).forEach((localeKey) => {
+    if (!missingKeys[localeKey]) missingKeys[localeKey] = [];
+    if (
+      !Object.keys(locales[localeKey]).includes(key) ||
+      FORCE_KEYS.includes(key)
+    ) {
       missingKeys[localeKey].push(key);
     }
-  })
-})
+  });
+});
 
 const translate = async (text, target) => {
   const translate = new Translate({
     credentials,
-    projectId: 'pixy-mood-tracker'
+    projectId: "pixy-mood-tracker",
   });
-  
+
   const [translation] = await translate.translate(text, target);
 
   console.log(`Text: ${text}`);
   console.log(`Translation: ${translation}`);
 
   return translation;
-}
+};
 
 (async () => {
   const result = {};
 
-  for(const localeKey in missingKeys) {
-    for(const index in missingKeys[localeKey]) {
-      const key = missingKeys[localeKey][index]
-      if(!result[localeKey]) result[localeKey] = {}
-      console.log('translating…', locales.en[key], localeKey, key, localeKey)
-      console.log('')
-      if(Array.isArray(locales.en[key])) {
-        const translations = await Promise.all(locales.en[key].map(text => translate(text, localeKey)))
-        console.log(translations)
-        result[localeKey][key] = translations
+  for (const localeKey in missingKeys) {
+    for (const index in missingKeys[localeKey]) {
+      const key = missingKeys[localeKey][index];
+      if (!result[localeKey]) result[localeKey] = {};
+      console.log("translating…", locales.en[key], localeKey, key, localeKey);
+      console.log("");
+      if (Array.isArray(locales.en[key])) {
+        const translations = await Promise.all(
+          locales.en[key].map((text) => translate(text, localeKey))
+        );
+        console.log(translations);
+        result[localeKey][key] = translations;
       } else {
-        result[localeKey][key] = await translate(locales.en[key], localeKey)
+        result[localeKey][key] = await translate(locales.en[key], localeKey);
       }
     }
   }
 
-  for(const localeKey in result) {
+  for (const localeKey in result) {
     const locale = {
       ...locales[localeKey],
       ...result[localeKey],
     };
-    fs.writeFileSync(path+localeKey+'.json', JSON.stringify(locale, null, 2))
-    console.log('saved', localeKey, locale)
+    fs.writeFileSync(
+      path + localeKey + ".json",
+      JSON.stringify(locale, null, 2)
+    );
+    console.log("saved", localeKey, locale);
   }
-})()
+})();
