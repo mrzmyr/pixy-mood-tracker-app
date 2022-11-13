febb3e24dfa79ea59958688f4d300903e1e07d9d
mrzmyr <moritz@devmood.com>
2022-11-13 10:53:50 +0100

    chore: fix build and user data ip

--- app.config.ts
+++ app.config.ts
@@ -5,25 +5,25 @@ const CONFIG = require('./app.json')
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   const _config: ExpoConfig = { ...CONFIG.expo };
-  
+
   const PROFILE = process.env.PROFILE || 'development';
   const isDevClient = process.env.DEV_CLIENT === 'true';
-  
-  if(isDevClient) {
+
+  if (isDevClient) {
     _config.name = 'Pixy Dev';
-    _config.ios!.bundleIdentifier = `${_config.ios!.bundleIdentifier}.dev`
-    _config.android!.package = `${_config.android!.package}.dev`
+    _config.ios!.bundleIdentifier = `com.devmood.pixymoodtracker.dev`
+    _config.android!.package = `com.devmood.pixymoodtracker.dev`
     _config.android!.icon = _config.icon = './assets/images/icon-dev.png';
   }
 
-  if(PROFILE === 'production') {
+  if (PROFILE === 'production') {
     _config.plugins = _config.plugins!.map((plugin: any) => {
-      if(_.isArray(plugin) && plugin[0] === 'onesignal-expo-plugin') {
+      if (_.isArray(plugin) && plugin[0] === 'onesignal-expo-plugin') {
         return ['onesignal-expo-plugin', { mode: 'production' }]
       }
       return plugin;
     });
   }
-  
+
   return _config;
 };
