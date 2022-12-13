3ee085dfa3ef29f7e1c320b562542beb708d96d2
mrzmyr <moritz@devmood.com>
2022-12-13 18:17:04 +0100

    feat: Add tip to message step in `Logger`

--- src/components/Logger/slides/SlideMessage.tsx
+++ src/components/Logger/slides/SlideMessage.tsx
@@ -1,20 +1,52 @@
-import { forwardRef, useEffect, useRef, useState } from "react";
-import { Keyboard, KeyboardAvoidingView, Platform, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { getLogEditMarginTop } from "@/helpers/responsive";
 import { t } from "@/helpers/translation";
 import useColors from "@/hooks/useColors";
-import { LogItem } from "@/hooks/useLogs";
+import { LogItem, RATING_MAPPING, useLogState } from "@/hooks/useLogs";
 import { useTemporaryLog } from "@/hooks/useTemporaryLog";
+import { getAverageMood } from "@/lib/utils";
+import dayjs, { Dayjs } from "dayjs";
+import { forwardRef, useEffect, useRef, useState } from "react";
+import { Keyboard, KeyboardAvoidingView, Platform, Text, View } from "react-native";
+import { HelpCircle } from "react-native-feather";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import DismissKeyboard from "../../DismisKeyboard";
 import LinkButton from "../../LinkButton";
 import TextArea from "../../TextArea";
 import { SlideHeadline } from "../components/SlideHeadline";
 import { Footer } from "./Footer";
+import { Card } from '@/components/Card'
 
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 const MAX_LENGTH = 10 * 1000;
+
+const getMoodValueYesterday = (today: Dayjs): number | null => {
+  const logState = useLogState()
+
+  if (logState.items.length === 0) {
+    return null
+  }
+
+  const yesterday = today.subtract(1, 'day')
+  const itemsYesterday = logState.items.filter(item => dayjs(item.dateTime).isSame(yesterday, 'day'))
+  const yesterdayAverageMood = getAverageMood(itemsYesterday)
+
+  if (yesterdayAverageMood === null) {
+    return null
+  }
+
+  return RATING_MAPPING[yesterdayAverageMood]
+}
+
+const getMoodValueNow = (): number | null => {
+  const tempLog = useTemporaryLog();
+
+  if (tempLog?.data?.rating === null) {
+    return null
+  }
+
+  return RATING_MAPPING[tempLog?.data?.rating]
+}
 
 export const SlideMessage = forwardRef(({
   onChange,
@@ -31,6 +63,7 @@ export const SlideMessage = forwardRef(({
   const marginTop = getLogEditMarginTop()
 
   const placeholder = useRef(t(`log_modal_message_placeholder_${randomInt(1, 6)}`))
+  const [showTips, setShowTips] = useState(false)
 
   const [shouldExpand, setShouldExpand] = useState(false);
 
@@ -43,6 +76,31 @@ export const SlideMessage = forwardRef(({
       r2.remove()
     }
   }, [])
+
+  const date = dayjs(tempLog.data.dateTime)
+  const todayMoodValue = getMoodValueNow()
+  const yesterdayMoodValue = getMoodValueYesterday(date)
+
+  let questions: string[] = [
+    placeholder.current,
+  ]
+
+  if (
+    todayMoodValue !== null &&
+    yesterdayMoodValue !== null
+  ) {
+    questions.push(t('log_messasge_hint_1', {
+      word: todayMoodValue > yesterdayMoodValue ? t('up') : t('down'),
+    }))
+  }
+
+  if (tempLog.data?.emotions?.length > 0) {
+    questions.push(
+      t('log_messasge_hint_2', {
+        words: tempLog.data.emotions.slice(0, 5).map(emotion => t(`log_emotion_${emotion}`)).join(', ').toLowerCase(),
+      })
+    )
+  }
 
   return (
     <>
@@ -67,8 +125,27 @@ export const SlideMessage = forwardRef(({
                 marginTop: marginTop,
               }}
             >
-              <SlideHeadline>{t('log_note_question')}</SlideHeadline>
-
+              <View
+                style={{
+                  flexDirection: "row",
+                  width: "100%",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <SlideHeadline>{t('log_note_question')}</SlideHeadline>
+                <LinkButton
+                  onPress={() => {
+                    setShowTips(!showTips)
+                  }}
+                  style={{
+                    marginBottom: -12,
+                    marginTop: -12,
+                  }}
+                >
+                  <HelpCircle width={22} color={colors.text} />
+                </LinkButton>
+              </View>
               <View
                 style={{
                   flexDirection: "column",
@@ -79,7 +156,6 @@ export const SlideMessage = forwardRef(({
               >
                 <TextArea
                   ref={ref}
-                  placeholder={placeholder.current}
                   value={tempLog?.data?.message}
                   onChange={onChange}
                   maxLength={MAX_LENGTH}
@@ -89,6 +165,34 @@ export const SlideMessage = forwardRef(({
                   }}
                 />
               </View>
+              {showTips && questions.length > 0 && (
+                <View
+                  style={{
+                  }}
+                >
+                  <Card
+                    title={t('log_message_hint_title')}
+                    style={{
+                      backgroundColor: colors.logCardBackground,
+                      marginTop: 16,
+                    }}
+                    onClose={() => setShowTips(false)}
+                    hasFeedback
+                    analyticsId="log_message_hint"
+                  >
+                    {questions.map((q, index) => (
+                      <Text
+                        key={`q-${index}`}
+                        style={{
+                          fontSize: 17,
+                          color: colors.textSecondary,
+                          marginTop: index === 0 ? 0 : 8,
+                        }}
+                      >{q}</Text>
+                    ))}
+                  </Card>
+                </View>
+              )}
             </View>
             <Footer>
               {showDisable && (
