7ec135e464e6ab81abab3d13d28716d49f9714fb
mrzmyr <moritz@devmood.com>
2022-12-31 15:39:33 +0100

    style: align tag border

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, View, ViewStyle } from "react-native";
+import { Pressable, Text, View, ViewStyle, useColorScheme } from "react-native";
 import useColors from "@/hooks/useColors";
 import useHaptics from "@/hooks/useHaptics";
 import { TAG_COLOR_NAMES } from "@/constants/Config";
@@ -18,12 +18,11 @@ export default function Tag({
 }) {
   const colors = useColors();
   const haptics = useHaptics();
+  const colorScheme = useColorScheme();
 
   return (
     <Pressable
       style={({ pressed }) => ({
-        paddingVertical: 8,
-        paddingHorizontal: 16,
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
@@ -31,8 +30,12 @@ export default function Tag({
         marginRight: 8,
         marginBottom: 8,
         backgroundColor: selected ? colors.tagBackgroundActive : colors.tagBackground,
-        borderWidth: 1,
-        borderColor: selected ? colors.tagBorderActive : colors.tagBorder,
+        borderColor: selected ? colors.tint : colorScheme === 'light' ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.2)',
+        borderWidth: selected ? 2 : 1,
+        paddingLeft: selected ? 15 : 16,
+        paddingRight: selected ? 15 : 16,
+        paddingTop: selected ? 7 : 8,
+        paddingBottom: selected ? 7 : 8,
         opacity: pressed ? 0.8 : 1,
         ...style,
       })}
