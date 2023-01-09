1a900d3504ce9bb56d204d5c92f9b2f00d34eeff
mrzmyr <moritz@devmood.com>
2023-01-09 19:34:59 +0100

    fix: emotions items

--- src/screens/LogList/EmotionItem.tsx
+++ src/screens/LogList/EmotionItem.tsx
@@ -11,28 +11,30 @@ export const EmotionItem = ({
   const colors = useColors();
 
   return (
-    <View
-      style={{
-        paddingVertical: 6,
-        paddingHorizontal: 12,
-        borderRadius: 8,
-        backgroundColor: colors.logCardBackground,
-        borderWidth: 1,
-        borderColor: colors.logCardBorder,
-        marginRight: 8,
-        marginBottom: 8,
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-      }}
-    >
-      <EmotionIndicator category={emotion.category} />
-      <Text
+    <View>
+      <View
         style={{
-          color: colors.text,
-          fontSize: 17,
+          paddingVertical: 6,
+          paddingHorizontal: 12,
+          borderRadius: 8,
+          backgroundColor: colors.logCardBackground,
+          borderWidth: 1,
+          borderColor: colors.logCardBorder,
+          marginRight: 8,
+          marginBottom: 8,
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
         }}
-      >{t(`log_emotion_${emotion.key}`)}</Text>
+      >
+        <EmotionIndicator category={emotion.category} />
+        <Text
+          style={{
+            color: colors.text,
+            fontSize: 17,
+          }}
+        >{t(`log_emotion_${emotion.key}`)}</Text>
+      </View>
     </View>
   );
 };
